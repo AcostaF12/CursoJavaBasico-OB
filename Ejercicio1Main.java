41699262e13220be46d5e1f0c57c1a9b9322d75f
AcostaF12 <107961278+AcostaF12@users.noreply.github.com>
2023-01-20 16:03:05 -0300

    Add files via upload

---
+++ Ejercicio1Main.java
@@ -0,0 +1,17 @@
+public class Ejercicio1Main {
+    
+    public static void main(String[] args) {
+        
+        byte numero1 = 2;
+        int numero2 = 10;
+        short numero3 = 30;
+        long numero4 = 100;
+        float numero5 = 100.99f;
+        double numero6 = 120.50d;
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        String nombre = "Acosta Federico";
+    }
+}
